464e82fc0d5135629e4f96e8781679f8428e629d
arsahak <arsahak99@gmail.com>
2025-08-20 12:06:08 +0600

    update

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,15 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    port: 3000,
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
